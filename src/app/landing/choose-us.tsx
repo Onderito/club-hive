a1c28ac3af99bbdcdcd4680a3a773939dec563e3
Onderito <ulas.onder@outlook.fr>
2025-07-07 16:47:11 +0200

    create choose us section

---
+++ src/app/landing/choose-us.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Image from "next/image";
+
+export default function ChooseUs() {
+  return (
+    <div>
+      <h2 className="text-white">Why choose us ? </h2>
+    </div>
+  );
+}
