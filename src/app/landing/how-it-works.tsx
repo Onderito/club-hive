014da26512c3aacad2a130f950a126323aa1302a
Onderito <ulas.onder@outlook.fr>
2025-07-08 12:46:19 +0200

    how it works, only 1 card more

--- src/app/landing/how-it-works.tsx
+++ src/app/landing/how-it-works.tsx
@@ -86,6 +86,40 @@ export default function HowItWorks() {
               </div>
             </div>
           )}
+          {index === 1 && (
+            <div className="shadow-sm shadow-[#585BFF] rounded-xl pt-4 pb-14 mt-8 bg-[#26263B]/45 ">
+              <div className="flex items-center justify-center gap-2">
+                <Image
+                  src={"/coach-alex.svg"}
+                  alt="Blur"
+                  width={35}
+                  height={35}
+                />
+                <h4 className="text-white font-montserrat font-bold">
+                  Coach Alex
+                </h4>
+              </div>
+              <div className="flex items-center justify-center gap-2 mt-4">
+                <button className="text-white/30 shadow-inner-both p-1 px-2 rounded-xl">
+                  player name or email adress
+                </button>
+                <button className="text-white font-montserrat shadow-sm shadow-[#585BFF] p-1 bg-[#181818] rounded-lg text-sm">
+                  Add
+                </button>
+              </div>
+              <div className="flex items-center justify-center gap-2 mt-3">
+                <button className="text-white/30 shadow-inner-both p-1 px-2 rounded-xl">
+                  player name or email adress
+                </button>
+                <button className="text-white font-montserrat shadow-sm shadow-[#585BFF] p-1 bg-[#181818] rounded-lg text-sm">
+                  Add
+                </button>
+              </div>
+              <button className="absolute text-white text-[12px] font-montserrat font-bold text-lg mt-4 -rotate-6 bg-[#181818] shadow-inner-color rounded-lg py-1 px-6 right-[120px] bottom-3 ">
+                Send invitation
+              </button>
+            </div>
+          )}
           <div className="w-35 h-35 bottom-0 right-0 absolute bg-gradient-to-r from-[#666688] to-[#585FFF] rounded-lg blur-[120px]"></div>
         </Card>
       ))}
